47199756d45a9e65f42cee980efc2ecc1f7d8b74
jack1142 <6032823+jack1142@users.noreply.github.com>
2018-04-10 22:32:42 +0200

    nuspec and chocolateyinstall files

---
+++ powerarchiver2016/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$packageArgs = @{
+  packageName            = 'powerarchiver2016'
+  fileType               = 'msi'
+  url                    = 'https://github.com/jack1142/choco-auto/releases/download/powerarchiver2016/setup.msi'
+  checksum               = 'f7e3629344287e963d7171d780bea153924e8ba269b543209993d5fd23c1d9f5'
+  checksumType           = 'sha256'
+  silentArgs             = '/qn /norestart'
+  validExitCodes         = @(0)
+  softwareName           = 'PowerArchiver 2016 *'
+}
+Install-ChocolateyPackage @packageArgs
