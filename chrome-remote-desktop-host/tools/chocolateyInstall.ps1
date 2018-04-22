24e2472d7a3bf81320afc24f43296859a7a030e0
jack1142 <6032823+jack1142@users.noreply.github.com>
2018-04-23 01:08:53 +0200

    dds-thumbnail-viewer and chrome-remote-desktop-host packages - nuspec, nupkg, install script

---
+++ chrome-remote-desktop-host/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$packageArgs = @{
+  packageName            = 'chrome-remote-desktop-host'
+  fileType               = 'msi'
+  url                    = 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi'
+  checksum               = '6418fd7b0f00b032227d59c0f5b483ed17a9384efd2544fc23b36ff269123a9f'
+  checksumType           = 'sha256'
+  silentArgs             = '/qn /norestart'
+  validExitCodes         = @(0)
+  softwareName           = 'Chrome Remote Desktop Host *'
+}
+Install-ChocolateyPackage @packageArgs
