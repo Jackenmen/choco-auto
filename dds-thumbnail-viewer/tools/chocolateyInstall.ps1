52bbbc3aff22341686f19c3e37c3c4f322942fcb
jack1142 <6032823+jack1142@users.noreply.github.com>
2018-04-23 01:08:26 +0200

    dds-thumbnail-viewer and chrome-remote-desktop-host packages - nuspec, nupkg, install script

---
+++ dds-thumbnail-viewer/tools/chocolateyInstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$packageArgs = @{
+  packageName            = 'dds-thumbnail-viewer'
+  fileType               = 'exe'
+  url                    = 'http://download.nvidia.com/developer/NVTextureSuite/DDS_viewer.exe'
+  checksum               = '57e65c30e8c38b09fff818e9636f6c2f9ac58b92bf6b802a7aa5156c1e75e6a7'
+  checksumType           = 'sha256'
+  silentArgs             = '/s'
+  validExitCodes         = @(0)
+  softwareName           = 'DDS Thumbnail Viewer *'
+}
+Install-ChocolateyPackage @packageArgs
