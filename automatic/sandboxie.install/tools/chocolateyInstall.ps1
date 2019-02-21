e608ba7f751b7d07549c4e38e744e26b114d45d3
Chocolatey <chocolatey@realdimensions.net>
2019-02-21 01:16:57 +0000

    AU: 2 updated - sandboxie sandboxie.install [skip ci] https://gist.github.com/4a1ffb06ffe2a5216cb65b16ebe279df/0fa082232008caf06513a82c360fa40d33717568

--- automatic/sandboxie.install/tools/chocolateyInstall.ps1
+++ automatic/sandboxie.install/tools/chocolateyInstall.ps1
@@ -11,8 +11,8 @@ if ($key) {
 $packageArgs = @{
   packageName            = 'sandboxie.install'
   fileType               = 'exe'
-  url                    = 'https://www.sandboxie.com/SandboxieInstall-527-3.exe'
-  checksum               = 'cfa5de62a43a1c01fd257989081632e02b343acdf8a1cadf5799a2e53a10ea7a'
+  url                    = 'https://www.sandboxie.com/attic/SandboxieInstall-528.exe'
+  checksum               = '4043c48b38c5fa77ed797f83a27e9a346dc5f559e1fae7f26096fa661a38b8e1'
   checksumType           = 'sha256'
   silentArgs             = $silentArgs
   validExitCodes         = @(0)
