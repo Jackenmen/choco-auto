31f0491caaa4d9b27b47283b2d83cf84b7ef6b70
Chocolatey <chocolatey@realdimensions.net>
2019-04-04 00:05:51 +0000

    AU: 1 updated - chrome-remote-desktop-host [skip ci] https://gist.github.com/4a1ffb06ffe2a5216cb65b16ebe279df/d99078b7b7f1a54ec0bbe565e5f57a48793c0a92

--- automatic/chrome-remote-desktop-host/tools/chocolateyInstall.ps1
+++ automatic/chrome-remote-desktop-host/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
  
 [array]$key = Get-UninstallRegistryKey -SoftwareName "Chrome Remote Desktop Host"
 $alreadyInstalled = $false
-$version = '71.0.3578.15'
+$version = '73.0.3683.67'
 
 if ($key.Count -ne 0) {
   $key | ForEach-Object {
@@ -16,7 +16,7 @@ $packageArgs = @{
   packageName            = 'chrome-remote-desktop-host'
   fileType               = 'msi'
   url                    = 'https://dl.google.com/dl/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi'
-  checksum               = '625b40a86abe0bbb41b694cc77f16415c4a0da173067498383638ab93cf59ac0'
+  checksum               = 'c9da1c5131d94453186e9b79cdfdd81ad468246f10008a81befaddd0a65efc93'
   checksumType           = 'sha256'
   silentArgs             = '/qn /norestart'
   validExitCodes         = @(0)
