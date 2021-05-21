f78e2b9828308fffd251428d8ffa153e7d223f02
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-05-21 14:51:34 +0200

    [sublimetext4] use a proper version string

--- automatic/sublimetext4/update.ps1
+++ automatic/sublimetext4/update.ps1
@@ -17,9 +17,19 @@ function global:au_BeforeUpdate {
 
 function global:au_GetLatest {
      $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-     $regex   = 'sublime_text_build_(4\d+)_x64_setup\.exe$'
+     $regex   = 'sublime_text_build_4\d+_x64_setup\.exe$'
      $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href
-     $version = $Matches[1]
+
+     $version_regex = '<h2>(?<version>[\d\.]+)\s*\(Build (?<build>4\d+)'
+     $null = $download_page.Content -match $version_regex
+     $version = $Matches.version
+     $build = $Matches.build
+     $segment_count = (Select-String -InputObject $version -Pattern '\.' -AllMatches).Matches.Count + 1
+     for ($i = $segment_count; $i -lt 3; $i++) {
+         $version += '.0'
+     }
+     $version += ".$($build)00"
+
      return @{ Version = $version; URL = $url }
 }
 
