5a4e230c32ff9ae03b8c34dc7cf52f955e411b5d
jack1142 <6032823+jack1142@users.noreply.github.com>
2021-07-14 23:41:49 +0200

    [sublimetext4] add dev stream

--- automatic/sublimetext4/update.ps1
+++ automatic/sublimetext4/update.ps1
@@ -1,13 +1,27 @@
 import-module au
 
-$updatesEndpoint = 'https://www.sublimetext.com/updates/4/stable_update_check'
 $downloadUrlFormat = 'https://download.sublimetext.com/sublime_text_build_{0}_x64_setup.exe'
+$availableStreams = [ordered] @{
+    stable = @{
+        title           = 'Sublime Text 4'
+        updatesEndpoint = 'https://www.sublimetext.com/updates/4/stable_update_check'
+        versionFormat   = '4.0.0.{0}00'
+    }
+    dev = @{
+        title           = 'Sublime Text 4'
+        updatesEndpoint = 'https://www.sublimetext.com/updates/4/dev_update_check'
+        versionFormat   = '4.0.0.{0}00-dev'
+    }
+}
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum)'"
+            "(?i)(^\s*url\s*=\s*)('.*')"         = "`$1'$($Latest.URL)'"
+            "(?i)(^\s*checksum\s*=\s*)('.*')"    = "`$1'$($Latest.Checksum)'"
+        }
+        "$($Latest.PackageName).nuspec" = @{
+            "(?i)(^\s*\<title\>).*(\<\/title\>)" = "`${1}$($Latest.Title)`${2}"
         }
     }
 }
@@ -16,12 +30,42 @@ function global:au_BeforeUpdate {
     $Latest.Checksum = Get-RemoteChecksum $Latest.URL
 }
 
-function global:au_GetLatest {
-     $releaseInformation = Invoke-RestMethod -Uri $updatesEndpoint
-     $buildNumber = $releaseInformation.latest_version
-     $downloadUrl = $downloadUrlFormat -f $buildNumber
+function Get-Stream {
+    param(
+        [string]$updatesEndpoint,
+        [string]$title,
+        [string]$versionFormat
+    )
 
-     return @{ Version = "4.0.0.$($buildNumber)00"; URL = $downloadUrl }
+    $releaseInformation = Invoke-RestMethod -Uri $updatesEndpoint
+    $buildNumber = $releaseInformation.latest_version
+
+    @{
+        Title   = $title
+        Version = $versionFormat -f $buildNumber
+        URL     = $downloadUrlFormat -f $buildNumber
+    }
+}
+
+function global:au_GetLatest {
+    $streams = [ordered] @{}
+    $errors = [ordered] @{}
+
+    ForEach ($stream in $availableStreams.GetEnumerator()) {
+        Try {
+            $streamData = $stream.Value
+            $streams[$stream.Name] = Get-Stream @streamData
+        }
+        Catch {
+            $streams[$stream.Name] = 'ignore'
+            $errors[$stream.Name] = $_
+        }
+    }
+
+    if ($errors.Count)
+        Write-Error $errors
+
+    return @{ Streams = $streams }
 }
 
 update -ChecksumFor none
