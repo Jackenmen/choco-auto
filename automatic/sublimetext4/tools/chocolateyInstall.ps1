7f564e96ec5075fdfee54790449e6e07198173be
Chocolatey <chocolatey@realdimensions.net>
2021-10-26 16:05:41 +0000

    AU: 1 updated - sublimetext4 [skip ci] https://gist.github.com/4a1ffb06ffe2a5216cb65b16ebe279df/2f1c585a8cad9a9194253c5cec7174d6743cb2e8

--- automatic/sublimetext4/tools/chocolateyInstall.ps1
+++ automatic/sublimetext4/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName            = 'sublimetext4'
   fileType               = 'exe'
-  url                    = 'https://download.sublimetext.com/sublime_text_build_4120_x64_setup.exe'
-  checksum               = '4fad8022dcf6f9201b4986f452a53cab6c06e05f63355f5a28898a562d73fece'
+  url                    = 'https://download.sublimetext.com/sublime_text_build_4121_x64_setup.exe'
+  checksum               = '6aa7b5ce3b967fc726abf69158dac98a826e1532e3832e3d8e649cbd877b0a6a'
   checksumType           = 'sha256'
   silentArgs             = '/VERYSILENT /NORESTART /TASKS="contextentry"'
   validExitCodes         = @(0)
