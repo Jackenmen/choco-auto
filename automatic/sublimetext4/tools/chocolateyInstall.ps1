0db7669a41e88a8f408348a3a9c333764d3c92e0
Jeremy Chadwick <jdc@koitsu.org>
2022-12-15 01:13:32 +0100

    [sublimetext4] fix uninstall scripts
    Sublime Text 4's uninstall registry key is named 'Sublime Text'
    rather than 'Sublime Text 4'. This does not affect Sublime Text 3
    installations as they seem to be using 'Sublime Text 3' name.

--- automatic/sublimetext4/tools/chocolateyInstall.ps1
+++ automatic/sublimetext4/tools/chocolateyInstall.ps1
@@ -1,14 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+$packageName = $env:ChocolateyPackageName
+$softwareName = 'Sublime Text'
+
 $packageArgs = @{
-  packageName            = 'sublimetext4'
+  packageName            = $packageName
   fileType               = 'exe'
   url                    = 'https://download.sublimetext.com/sublime_text_build_4145_x64_setup.exe'
   checksum               = '42e50b59c5ae6d803a6ac87ccf12b0d215572bfdfc39b8f9448085b6f97d290c'
   checksumType           = 'sha256'
   silentArgs             = '/VERYSILENT /NORESTART /TASKS="contextentry"'
   validExitCodes         = @(0)
-  softwareName           = 'Sublime Text 4'
 }
 
 Install-ChocolateyPackage @packageArgs
