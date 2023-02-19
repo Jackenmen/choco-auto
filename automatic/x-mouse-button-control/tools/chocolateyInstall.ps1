17806a91d7c84e8ad9334a7070eafa85cff22a1e
Chocolatey <chocolatey@realdimensions.net>
2023-02-19 16:06:28 +0000

    AU: 1 updated - x-mouse-button-control [skip ci] https://gist.github.com/4a1ffb06ffe2a5216cb65b16ebe279df/1fbe8891f22d33fa2683da957d6a035ae49a03eb

--- automatic/x-mouse-button-control/tools/chocolateyInstall.ps1
+++ automatic/x-mouse-button-control/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
     packageName            = $env:ChocolateyPackageName
     fileType               = 'exe'
-    url                    = 'https://dvps.highrez.co.uk/downloads/XMouseButtonControlSetup.2.19.2.exe'
-    checksum               = 'bc99080acc10eeb1c8379719c86c652221f3f6d1bff104a2ca32d6326154c636'
+    url                    = 'https://dvps.highrez.co.uk/downloads/XMouseButtonControlSetup.2.20.exe'
+    checksum               = '3e7eff7f09134c46d0ed2e01f28a95eaa96b03246986c7d77b7466aab50c9f7e'
     checksumType           = 'sha256'
     silentArgs             = '/S'
     validExitCodes         = @(0)
