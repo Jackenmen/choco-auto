3ec7eb6b352cc0b0fd1c1fd1a8705aa750790712
Chocolatey <chocolatey@realdimensions.net>
2024-07-13 16:07:05 +0000

    AU: 4 updated - sandboxie sandboxie.install sandboxie-plus sandboxie-plus.install [skip ci] https://gist.github.com/Jackenmen/4a1ffb06ffe2a5216cb65b16ebe279df/993e71d307eb63ff0e8db5da5926fc71607c24ed

--- automatic/sandboxie-plus.install/tools/chocolateyInstall.ps1
+++ automatic/sandboxie-plus.install/tools/chocolateyInstall.ps1
@@ -14,8 +14,8 @@ if ($pp.Language) {
 $packageArgs = @{
   packageName            = 'sandboxie-plus.install'
   fileType               = 'exe'
-  file                   = "$toolsPath\Sandboxie-Plus-x86-v1.14.3.exe"
-  file64                 = "$toolsPath\Sandboxie-Plus-x64-v1.14.3.exe"
+  file                   = "$toolsPath\Sandboxie-Plus-x86-v1.14.4.exe"
+  file64                 = "$toolsPath\Sandboxie-Plus-x64-v1.14.4.exe"
   silentArgs             = $silentArgs
   validExitCodes         = @(0)
   softwareName           = 'Sandboxie-Plus *'
--- automatic/sandboxie.install/tools/chocolateyInstall.ps1
+++ automatic/sandboxie.install/tools/chocolateyInstall.ps1
@@ -28,8 +28,8 @@ if ($pp.Language) {
 $packageArgs = @{
   packageName            = 'sandboxie.install'
   fileType               = 'exe'
-  file                   = "$toolsPath\Sandboxie-Classic-x86-v5.69.3.exe"
-  file64                 = "$toolsPath\Sandboxie-Classic-x64-v5.69.3.exe"
+  file                   = "$toolsPath\Sandboxie-Classic-x86-v5.69.4.exe"
+  file64                 = "$toolsPath\Sandboxie-Classic-x64-v5.69.4.exe"
   silentArgs             = $silentArgs
   validExitCodes         = @(0)
   softwareName           = 'Sandboxie *'
