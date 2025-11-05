e989f5424294bff9b9405c05daf8d3b7e2de5b10
Chocolatey <chocolatey@realdimensions.net>
2025-11-05 00:11:25 +0000

    AU: 4 updated - sandboxie sandboxie.install sandboxie-plus sandboxie-plus.install [skip ci] https://gist.github.com/Jackenmen/4a1ffb06ffe2a5216cb65b16ebe279df/08b1946787a11e9b889b618bbb376928117b5327

--- automatic/sandboxie.install/tools/chocolateyInstall.ps1
+++ automatic/sandboxie.install/tools/chocolateyInstall.ps1
@@ -28,7 +28,7 @@ if ($pp.Language) {
 $packageArgs = @{
   packageName            = 'sandboxie.install'
   fileType               = 'exe'
-  file64                 = "$toolsPath\Sandboxie-Classic-x64-v5.71.4.exe"
+  file64                 = "$toolsPath\Sandboxie-Classic-x64-v5.71.5.exe"
   silentArgs             = $silentArgs
   validExitCodes         = @(0)
   softwareName           = 'Sandboxie *'
